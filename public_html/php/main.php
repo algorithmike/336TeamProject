7d6f09dc6d5ea6cef9f57699ad35d69cf4350f30
Michael <micfernandez@csumb.edu>
2016-11-06 05:14:33 +0000

    Created database and first table with 40 stuff in it

--- public_html/php/main.php
+++ public_html/php/main.php
@@ -5,7 +5,28 @@
     </head>
     <body>
         <div class="wrapper">
-            
+            <?php
+                //CONNECT TO DB
+                $host = 'localhost';
+                $user = 'project2_is_coo';
+                $password = 'a1b2xyZ123';
+                $database = 'project2_is_coo';
+                $dbConn = new PDO('mysql:host='.$host.';dbname='.$database, $user, $password);
+    			
+                //QUERY
+                $table1 = 'general_info';
+                $sqlquery = 'SELECT * FROM '.$table1.' ORDER BY animal_ID ASC;';
+                $statement = $dbConn -> prepare($sqlquery);
+                $statement -> execute();
+                
+                //SET UP HTML TABLE, FETCH AND DISPLAY DATA
+                echo '<h1>Everything In This Table: general_info</h1>'.$sqlquery.'<table>';
+                echo '<tr><td style="font-weight: bold">ID</td><td style="font-weight: bold">Name</td><td style="font-weight: bold">Description</td></tr>';
+                while($row = $statement -> fetch()){
+                    echo '<tr><td>'.$row["animal_ID"].'</td><td>'.$row["name"].'</td><td>'.$row["description"].'</td></tr>';
+                }
+                echo '</table>';
+            ?>
         </div>
     </body>
 </html>

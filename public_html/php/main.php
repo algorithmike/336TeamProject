f0b2290c733f7026e9a89924b18c9358523c2fc5
Michael <micfernandez@csumb.edu>
2016-11-06 07:21:26 +0000

    created 3rd table. done initializing database and tables

--- public_html/php/main.php
+++ public_html/php/main.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <title>Project 2</title>
+        <link rel="stylesheet" type="text/css" href="../css/styles.css/">
     </head>
     <body>
         <div class="wrapper">
@@ -20,7 +21,7 @@
                 $statement -> execute();
                 
                 //SET UP HTML TABLE, FETCH AND DISPLAY DATA
-                echo '<h1>Everything In This Table: detailed_info</h1>'.$sqlquery.'<table>';
+                echo '<h1>Table: detailed_info</h1>'.$sqlquery.'<table>';
                 echo '<tr><td style="font-weight: bold">ID</td><td style="font-weight: bold">Name</td><td style="font-weight: bold">Description</td></tr>';
                 while($row = $statement -> fetch()){
                     echo '<tr><td>'.$row["animal_ID"].'</td><td>'.$row["name"].'</td><td>'.$row["description"].'</td></tr>';
@@ -34,10 +35,24 @@
                 $statement -> execute();
                 
                 //SET UP HTML TABLE, FETCH AND DISPLAY DATA
-                echo '<h1>Everything In This Table: general_info</h1>'.$sqlquery.'<table>';
+                echo '<h1>Table: general_info</h1>'.$sqlquery.'<table>';
                 echo '<tr><td style="font-weight: bold">ID</td><td style="font-weight: bold">Gender</td><td style="font-weight: bold">Color</td><td style="font-weight: bold">Size</td><td style="font-weight: bold">Age</td></tr>';
                 while($row = $statement -> fetch()){
                     echo '<tr><td>'.$row["animal_ID"].'</td><td>'.$row["gender"].'</td><td>'.$row["color"].'</td><td>'.$row["size"].'</td><td>'.$row["age"].'</td></tr>';
+                }
+                echo '</table>';
+                
+                //QUERY
+                $table1 = 'animal_cost';
+                $sqlquery = 'SELECT * FROM '.$table1.' ORDER BY animal_ID ASC;';
+                $statement = $dbConn -> prepare($sqlquery);
+                $statement -> execute();
+                
+                //SET UP HTML TABLE, FETCH AND DISPLAY DATA
+                echo '<h1>Table: animal_cost</h1>'.$sqlquery.'<table>';
+                echo '<tr><td style="font-weight: bold">ID</td><td style="font-weight: bold">Adoption Fee</td></tr>';
+                while($row = $statement -> fetch()){
+                    echo '<tr><td>'.$row["animal_ID"].'</td><td>$'.$row["adoption_fee"].'</td></tr>';
                 }
                 echo '</table>';
             ?>

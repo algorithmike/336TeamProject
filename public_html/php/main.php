7f18faac83f28f8338741139a75d11c32d022747
Michael <micfernandez@csumb.edu>
2016-11-08 02:34:15 +0000

    checked off items on rubric

--- public_html/php/main.php
+++ public_html/php/main.php
@@ -1,14 +1,14 @@
 <!DOCTYPE HTML>
 <html>
-<!--1) Database has at least 3 tables with 40 records (10 points)-->
-<!--2) Users can filter data using at least three fields (15 points)-->
-<!--3) Users can sort results (asc,desc) using at least one field (10 points)-->
-<!--4) Users can click on an item to get further info (10 points)-->
+<!--1) Database has at least 3 tables with 40 records (10 points) ***********DONE*********** -->
+<!--2) Users can filter data using at least three fields (15 points) ***********MIKE IS WORKING ON THIS*********** -->
+<!--3) Users can sort results (asc,desc) using at least one field (10 points) ***********MIKE IS WORKING ON THIS***********-->
+<!--4) Users can click on an item to get further info (10 points) ***********MIKE IS WORKING ON THIS***********-->
 <!--5) Users can add items to shopping cart using a Session (10 points)-->
 <!--6) Users can see the content of the shopping cart (10 points)-->
 <!--7) The web pages have a nice and consistent look and feel (10 points)-->
-<!--8) The team used Github for collaboration (10 points)-->
-<!--9) The team used Trello or a similar tool for project management (10 points)-->
+<!--8) The team used Github for collaboration (10 points) ***********DONE***********-->
+<!--9) The team used Trello or a similar tool for project management (10 points) ***********DONE***********-->
 <!--10) In a Word document include User Story, Database schema, and mock up (5 points) UPLOAD these documents here and ALSO link them from your C9 site  (5 points)-->
     <head>
         <title>Project 2</title>

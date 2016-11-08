ccf685e5bcfa716b0bf8484289a5c06cb77ef744
Michael <micfernandez@csumb.edu>
2016-11-08 20:15:53 +0000

    minor update

--- public_html/php/main.php
+++ public_html/php/main.php
@@ -3,7 +3,7 @@
 <!--1) Database has at least 3 tables with 40 records (10 points) ***********DONE*********** -->
 <!--2) Users can filter data using at least three fields (15 points) ***********DONE*********** -->
 <!--3) Users can sort results (asc,desc) using at least one field (10 points) ***********DONE***********-->
-<!--4) Users can click on an item to get further info (10 points) ***********MIKE IS WORKING ON THIS***********-->
+<!--4) Users can click on an item to get further info (10 points) ***********CHECK OUT THE JAVASCRIPT FUNCTION showDescription()***********-->
 <!--5) Users can add items to shopping cart using a Session (10 points)-->
 <!--6) Users can see the content of the shopping cart (10 points)-->
 <!--7) The web pages have a nice and consistent look and feel (10 points)-->
